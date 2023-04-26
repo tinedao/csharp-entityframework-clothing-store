d8f29dba638944a8cc6d1d812c9080fbabcf3786
Tine Đào <122794534+tinedao@users.noreply.github.com>
2023-04-26 12:48:34 +0700

    someview

--- ClothingStoreMVC/Models/Product.cs
+++ ClothingStoreMVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ClothingStoreMVC.Models
 {
@@ -11,15 +12,23 @@ namespace ClothingStoreMVC.Models
             Wishlist = new HashSet<Wishlist>();
         }
 
+        [DisplayName("Mã sản phẩm")]
         public int Id { get; set; }
+        [DisplayName("Tên sản phẩm")]
         public string Name { get; set; }
+        [DisplayName("Đánh giá")]
         public double? Star { get; set; }
+        [DisplayName("Giá")]
         public decimal? Price { get; set; }
+        [DisplayName("Kích cỡ")]
         public string Size { get; set; }
+        [DisplayName("Màu")]
         public string Color { get; set; }
+        [DisplayName("Số lượng")]
         public int? Quantity { get; set; }
         public int? IdCate { get; set; }
-
+        [DisplayName("Loại")]
+        public virtual Category Category { get; set; }
         public Category IdCateNavigation { get; set; }
         public ICollection<Cart> Cart { get; set; }
         public ICollection<Wishlist> Wishlist { get; set; }
